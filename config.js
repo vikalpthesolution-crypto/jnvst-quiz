3c40fdb0bb3f47b33316e9ad974579a569526081
vikalpthesolution-crypto <vikalpthesolution@gmail.com>
2026-08-12 23:35:15 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// JNVST EVS Website Configuration
+// अपने Google Apps Script Web App का /exec URL नीचे quotes के बीच paste करें.
+window.JNVST_CONFIG = {
+  googleScriptUrl: "",
+  siteTitle: "JNVST EVS Online Test"
+};
